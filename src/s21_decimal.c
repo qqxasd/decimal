b47b988a60665aca0a5ca2c03b80750e4eb74e50
Nikita <nikit@nikita.localdomain>
2023-05-12 21:16:21 +0300

    correct basic functions

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -2,9 +2,8 @@
 
 int main() {
   s21_decimal decimal;
-  decimal.bits[3] = 0x0000F008;
-  decimal.bits[3] = toggleBit(decimal.bits[3], 3);
-  printf("%d", decimal.bits[3]);
+  decimal.bits[3] = 0x801C0000;
+  printf("%d %d %d", decimal.bits[3], get_exp(decimal), decimal.bits[3]);
   return 0;
 }
 
@@ -14,5 +13,5 @@ int clearBit(int num, int pos) { return (num & (~(1 << pos))); }
 
 int toggleBit(int num, int pos) { return (num ^ (1 << pos)); }
 
-int get_exp(s21_decimal value) { return ((value.bits[3] < 1) > 17); }
-int get_sign(s21_decimal value) { return (value.bits[3] >> 1) & 1; }
+int get_exp(s21_decimal value) { return (value.bits[3] << 10) >> 26; }
+int get_sign(s21_decimal value) { return (value.bits[3] >> 31) & 1; }
