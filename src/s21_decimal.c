5b0ad32660a5f045d05361b316f7f5bddde52207
Nikita <nikit@nikita.localdomain>
2023-05-12 20:13:13 +0300

    some new functions

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -1,8 +1,18 @@
 #include "s21_decimal.h"
 
-int get_exp(s21_decimal value) {
-    return ((value.bits[3] < 1) > 17);
+int main() {
+  s21_decimal decimal;
+  decimal.bits[3] = 0x0000F008;
+  decimal.bits[3] = toggleBit(decimal.bits[3], 3);
+  printf("%d", decimal.bits[3]);
+  return 0;
 }
-int get_sign(s21_decimal value) {
-    return (value.bits[3] < 1) & 1;
-}
+
+int setBit(int num, int pos) { return (num | (1 << pos)); }
+
+int clearBit(int num, int pos) { return (num & (~(1 << pos))); }
+
+int toggleBit(int num, int pos) { return (num ^ (1 << pos)); }
+
+int get_exp(s21_decimal value) { return ((value.bits[3] < 1) > 17); }
+int get_sign(s21_decimal value) { return (value.bits[3] >> 1) & 1; }
--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -1,45 +1,54 @@
-#ifndef S21_DECIMAL.H
-#define S21_DECIMAL.H
+#ifndef S21_DECIMAL_H
+#define S21_DECIMAL_H
 
-#include <stdio.h>
 #include <check.h>
+#include <stdio.h>
 
-typedef struct 
-{
-    int bits[4];
+typedef struct {
+  int bits[4];
 } s21_decimal;
 
-int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result); // арифмтеичексие операции Никита (Кайфует)
-int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result); //
-int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result); //
-int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result); //
+int s21_add(s21_decimal value_1, s21_decimal value_2,
+            s21_decimal *result);  // арифмтеичексие операции Никита (Кайфует)
+int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);  //
+int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);  //
+int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);  //
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int s21_is_less(s21_decimal, s21_decimal);           //Сравнение Тим (кайфует )
-int s21_is_less_or_equal(s21_decimal, s21_decimal);      //
-int s21_is_greater(s21_decimal, s21_decimal);            // 
+int s21_is_less(s21_decimal, s21_decimal);  // Сравнение Тим (кайфует )
+int s21_is_less_or_equal(s21_decimal, s21_decimal);     //
+int s21_is_greater(s21_decimal, s21_decimal);           //
 int s21_is_greater_or_equal(s21_decimal, s21_decimal);  //
 int s21_is_equal(s21_decimal, s21_decimal);             //
-int s21_is_not_equal(s21_decimal, s21_decimal);          //
+int s21_is_not_equal(s21_decimal, s21_decimal);         //
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int s21_from_int_to_decimal(int src, s21_decimal *dst);         //Переводы Денис (двигается на блессе)
-int s21_from_float_to_decimal(float src, s21_decimal *dst);     //
-int s21_from_decimal_to_int(s21_decimal src, int *dst);         //
-int s21_from_decimal_to_float(s21_decimal src, float *dst);     //
+int s21_from_int_to_decimal(
+    int src, s21_decimal *dst);  // Переводы Денис (двигается на блессе)
+int s21_from_float_to_decimal(float src, s21_decimal *dst);  //
+int s21_from_decimal_to_int(s21_decimal src, int *dst);      //
+int s21_from_decimal_to_float(s21_decimal src, float *dst);  //
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int s21_floor(s21_decimal value, s21_decimal *result);      // Дополнительные фукнции Санан (радуется что дописал принтф)
-int s21_round(s21_decimal value, s21_decimal *result);      // кайфует нереально!!!
-int s21_truncate(s21_decimal value, s21_decimal *result);   // реально самый счастливый если успеет дописать принтф
-int s21_negate(s21_decimal value, s21_decimal *result);     // )))))
+int s21_floor(s21_decimal value,
+              s21_decimal *result);  // Дополнительные фукнции Санан (радуется
+                                     // что дописал принтф)
+int s21_round(s21_decimal value, s21_decimal *result);  // кайфует нереально!!!
+int s21_truncate(s21_decimal value,
+                 s21_decimal *result);  // реально самый счастливый если успеет
+                                        // дописать принтф
+int s21_negate(s21_decimal value, s21_decimal *result);  // )))))
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int get_exp(s21_decimal value);
 int get_sign(s21_decimal value);
+int setBit(int num, int pos);
 
+int clearBit(int num, int pos);
+
+int toggleBit(int num, int pos);
 #endif
