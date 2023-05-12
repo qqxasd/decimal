af769f8c18708c23f80b14d15fa246181ae1098f
Nikita <nikit@nikita.localdomain>
2023-05-12 21:44:02 +0300

    correct basic functions

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -1,12 +1,5 @@
 #include "s21_decimal.h"
 
-int main() {
-  s21_decimal decimal;
-  decimal.bits[3] = 0x801C0000;
-  printf("%d %d %d", decimal.bits[3], get_exp(decimal), decimal.bits[3]);
-  return 0;
-}
-
 int setBit(int num, int pos) { return (num | (1 << pos)); }
 
 int clearBit(int num, int pos) { return (num & (~(1 << pos))); }
