8e00012f8014b3e629ad5846cfb8d819653cc915
Heide Dragon <heidedra@su-e5.msk.21-school.ru>
2023-06-04 15:02:56 +0300

    minor

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -766,6 +766,7 @@ int s21_from_decimal_to_float(s21_decimal src, float *dst) {
   *dst *= pow(10, count_digits(src.bits[1]));
   *dst += src.bits[0];
   *dst /= pow(10, get_exp(src));
+  if (get_sign(src)) *dst *= -1;
   return 0;
 }
 
